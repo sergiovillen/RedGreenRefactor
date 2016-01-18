47927d6a62c4bf47929ad92058ccd212b1df387e
Sergio <sergio.villen.galera@hotmail.fr>
2016-01-18 21:15:19 +0100

    Red phase

---
+++ RedGreenRefactor/PairNumberTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace RedGreenRefactor
+{
+    public class PairNumberTests
+    {
+        [Test]
+        public void Given_2_then_return_true()
+        {
+            //Arrange
+            var pairNumber = new PairNumber();
+            //Act
+            var actual = pairNumber.IsPair(2);
+            //Assert
+            Assert.AreEqual(true, actual);
+        }
+    }
+
+    public class PairNumber
+    {
+        public bool IsPair(int input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
