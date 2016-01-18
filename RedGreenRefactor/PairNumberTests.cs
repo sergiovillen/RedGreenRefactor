7395823a21f16cc1caa562847595443d8f16721e
Sergio <sergio.villen.galera@hotmail.fr>
2016-01-18 21:25:40 +0100

    Adding nex scenarios without refactoring

--- RedGreenRefactor/PairNumberTests.cs
+++ RedGreenRefactor/PairNumberTests.cs
@@ -9,15 +9,17 @@ namespace RedGreenRefactor
 {
     public class PairNumberTests
     {
-        [Test]
-        public void Given_2_then_return_true()
+        [TestCase(1, false)][TestCase(2, true)][TestCase(3, false)][TestCase(4, true)]
+        [TestCase(5, false)][TestCase(6, true)][TestCase(7, false)][TestCase(8, true)]
+        [TestCase(9, false)][TestCase(10, true)][TestCase(11, false)][TestCase(12, true)]
+        public void Given_2_then_return_true(int input, bool expected)
         {
             //Arrange
             var pairNumber = new PairNumber();
             //Act
-            var actual = pairNumber.IsPair(2);
+            var actual = pairNumber.IsPair(input);
             //Assert
-            Assert.AreEqual(true, actual);
+            Assert.AreEqual(expected, actual);
         }
     }
 
@@ -25,7 +27,18 @@ namespace RedGreenRefactor
     {
         public bool IsPair(int input)
         {
+            if (input == 1) return false;
             if (input == 2) return true;
+            if (input == 3) return false;
+            if (input == 4) return true;
+            if (input == 5) return false;
+            if (input == 6) return true;
+            if (input == 7) return false;
+            if (input == 8) return true;
+            if (input == 9) return false;
+            if (input == 10) return true;
+            if (input == 11) return false;
+            if (input == 12) return true;            
             throw new NotImplementedException();
         }
     }
